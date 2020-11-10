45050e8cc9a24b701138b07b644d285dfc2fad71
paulday966 <paulday360@outlook.com>
2020-11-10 12:52:15 +0000

    Adding lines of JS to see how JSON effects the data from the websites API

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let xhr = new XMLHttpRequest();
+let data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        data = this.responseText;
+        /*console.log(JSON.parse(this.responseText));*/
+        /*document.getElementById("data").innerHTML = this.responseText;*/
+        /*console.log(typeof(JSON.parse(this.responseText)));*/
+        /*console.log(typeof(this.responseText));*/
     }
 };
+
+console.log(data);
