d94d24358ca8cc23c9690ec5383b0b8dd61a9ad3
paulday966 <paulday360@outlook.com>
2020-11-14 17:26:58 +0000

    Added code for the lesson

--- main3.js
+++ main3.js
@@ -3,9 +3,6 @@ const baseURL = "https://ci-swapi.herokuapp.com/api/";
 function getData(type, cb) {
     let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
-
     xhr.onreadystatechange = function() {
     /*console.log(this.readyState);*/ /* Will see more than one log to the console and will happen every single time the function is called and
     /* not just when the readyState is equal to 4 and the status is equal to 200*/
@@ -13,6 +10,10 @@ function getData(type, cb) {
             cb(JSON.parse(this.responseText)); // At this point it's goint to run the function that we passed it to getData as an argument
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
 }
 
 function getTableHeaders(obj) {
@@ -34,18 +35,17 @@ function writeToDocument(type) {
         data = data.results;
         let tableHeaders = getTableHeaders(data[0]);
 
+        //data.forEach(function(item) {
+            // el.innerHTML += "<p>" + InputEvent.name + "</p>";
+
         data.forEach(function(item) {
-            el.innerHTML += "<p>" + InputEvent.name + "</p>";
-
-        /*data.forEach(function(item) {
-            Object.keys(item).forEach(function(key) {
-                console.log(key);
-            })
+            //Object.keys(item).forEach(function(key) {
+                //console.log(key);
+            });
             /*document.getElementById("data").innerHTML += "<p>" + item.name + "</p>";*/
-            /*el.innerHTML += "<p>" + item.name + "</p>";*/
-        });
+            // el.innerHTML += "<p>" + item.name + "</p>";
 
-        el.innerHTML = `<table>${tableHeaders}</table>`
+        el.innerHTML = `<table>${tableHeaders}</table>`;
 
         /*document.getElementById("data").innerHTML = data.results;*/
     });
